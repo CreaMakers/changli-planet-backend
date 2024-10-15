5d8191500bf88b464ef9c6d75f908b3e25d35520
yuxialuozi <3159452953@qq.com>
2024-10-16 00:01:16 +0800

    feat:module frame

---
+++ tool-system/src/main/java/com/creamakers/toolsystem/ToolSystemApplication.java
@@ -0,0 +1,13 @@
+package com.creamakers.toolsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToolSystemApplication.class, args);
+	}
+
+}
---
+++ tool-system/src/test/java/com/creamakers/toolsystem/ToolSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creamakers.toolsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
