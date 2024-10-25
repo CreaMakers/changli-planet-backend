b2d7f581e871050529e1058f6b83983b7d801568
1973526541 <1973526541@qq.com>
2024-10-25 22:41:07 +0800

    全局异常拦截器

--- user-system/src/main/java/com/creamakers/usersystem/exception/GlobalExceptionHandler.java
+++ user-system/src/main/java/com/creamakers/usersystem/exception/GlobalExceptionHandler.java
@@ -1,16 +1,35 @@
 package com.creamakers.usersystem.exception;
 
+import com.creamakers.usersystem.consts.HttpCode;
+import com.creamakers.usersystem.dto.response.GeneralResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@Slf4j
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<String> handleRuntimeException(RuntimeException ex) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+    /**
+     * 漏掉的异常会走这里
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(value = Throwable.class)
+    public GeneralResponse<?> throwable(Throwable e) {
+        log.error("system exception! The reason is: {}",e.getMessage(),e);
+        return GeneralResponse.builder()
+                .data("全局异常错误")
+                .code(HttpCode.INTERNAL_SERVER_ERROR)
+                .build();
     }
 
 }
