6545ce6a36ddf8dc90d867ec24eaa91ee5a885e4
lclc0123 <535874837@qq.com>
2024-12-09 12:19:58 +0800

    feat: add report review processing feature

---
+++ web-system/src/main/java/com/creamakers/websystem/controller/ReportController.java
@@ -0,0 +1,39 @@
+package com.creamakers.websystem.controller;
+
+import com.creamakers.websystem.domain.dto.ReportPost;
+import com.creamakers.websystem.domain.dto.ReportUser;
+import com.creamakers.websystem.domain.vo.ResultVo;
+import com.creamakers.websystem.domain.vo.request.ReportUserReq;
+import com.creamakers.websystem.domain.vo.request.UserPunishmentReq;
+import com.creamakers.websystem.service.ReportService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/web/reports")
+public class ReportController {
+  @Autowired
+  private ReportService reportService;
+
+  /**
+   * 获取所有用户对用户的举报
+   * @return
+   */
+  @GetMapping("/users")
+  public ResultVo<List<ReportUser>> getAllReports(){
+    return reportService.getAllReports();
+
+
+
+
+  }
+
+  @PutMapping("/posts/{reportId}")
+  public ResultVo<ReportUser>  updateReportById(@PathVariable String reportId, @RequestBody ReportUserReq reportUserReq){
+    return reportService.updateReportById(reportId,reportUserReq);
+  }
+
+
+}
