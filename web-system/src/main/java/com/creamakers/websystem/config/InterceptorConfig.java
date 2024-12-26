c075d44bb2636f7b99e7bca68bc74e8100af4b8b
lclc0123 <535874837@qq.com>
2024-12-26 17:16:59 +0800

    解决合并冲突

--- web-system/src/main/java/com/creamakers/websystem/config/InterceptorConfig.java
+++ web-system/src/main/java/com/creamakers/websystem/config/InterceptorConfig.java
@@ -12,7 +12,7 @@ public class InterceptorConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(authenticationInterceptor)
-                .addPathPatterns("/**").excludePathPatterns("/**");
-               //.excludePathPatterns("/web/users/login","web/**");
+                .addPathPatterns("/**")
+               .excludePathPatterns("/web/users/login");
     }
 }
